2146df882bc2a4c599e08a6ea88b9ea5ac0824b5
Abndrey <160252981+Abndrey@users.noreply.github.com>
2025-03-11 11:47:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+alert ("Programa para saber se vc passou de ano");
+
+// var primeiroNumero = Number (prompt ("Digite um número"));
+// var segundoNumero = Number (prompt("Digite seu segundo número"));
+// var operacao = (prompt("Digite a operação a ser realizada"))
+var nomeDoAluno = "";
+var primeiraNota = 0;
+var segundaNota = 0;
+var terceiraNota = 0;
+var contador = 0;
+
+while(contador < 3){
+nomeDoAluno = prompt("Digite o nome do aluno");
+ primeiraNota =  Number (prompt("Digite a primeira nota")).
+ segundaNota = Number (prompt("Digite a segunda nota"));
+ terceiraNota = Number (prompt("Digite a terceira nota"));
+ contador
+}
+
+
+var media = (nota1 + nota2 + nota3) /3 ; 
+
+if ( media >= 6.0 ) {
+alert("Aprovado");
+
+}else{
+    alert("Você reprovou")
+}
+
+
+
+
+var media, nota1, nota2, nota3 ;
+
+aluno1 = prompt("Informe o nome do primeiro aluno:")
+nota1= prompt ("Informe a primeira nota: ") ;
+nota1= eval (nota1) ;
+
+aluno1 = prompt("Informe o nome do primeiro aluno:")
+nota2= prompt ("Informe a segunda nota: ") ;
+nota2= eval (nota2) ; 
+
+aluno1 = prompt("Informe o nome do primeiro aluno:")
+nota3= prompt ("Informe a terceira nota: ") ; 
+nota3= eval (nota3) ;
+
+
+
+
+
+
+
+
+
+// case "+" : var  = primeiroNumero + segundoNumero;
+    // break;
+
+    // case "-" : var subtracao = primeiroNumero + segundoNumero;
+    // alert("A soma é:"- subtracao);
+    // break;
+    // case "*" : var multiplicacao = primeiroNumero + segundoNumero;
+    // alert("A soma é:"* multiplicacao);
+    // break;
+    // case "/" : var divisao = primeiroNumero + segundoNumero;
+    // alert("A soma é:"/ divisao);
+    // break;
+
+
+
+
+
+// var userNumber = prompt("Digite o primeiro número positivo ou negativo");
+
+// console.log(userNumber);
+
+// var userNumber1 = prompt("Digite o segundo número positivo ou negativo");
+
+// var userNumber = 0;
+// userNumber = prompt("Digite um número para verificar se é positivo ou negativo: ");
+// if(userNumber > 0){
+//     alert(`Seu número ${userNumber} é um número positivo!`);
+// }
+// if(userNumber < 0){
+//     alert(`Seu número ${userNumber} é um número negativo!`);
+// }
+
+
+// Verificar se um número é positivo, negativo ou zero.
+
+// var primNum = prompt("Digite o primeiro número da soma");
+
+// var segundoNum = prompt("Digite o segundo número da soma");
+
+// console.log(segundoNum);
+
+// var  resultado =  "Resultado :" + Number (prinNumero) + Number (segundoNum);
+ 
+// console.log(resultado);
